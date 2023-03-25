3ae377c10938f0e82162bcfc65b22ebc615fdf16
mauro-balades <mauro.balades@tutanota.com>
2023-03-25 17:11:13 +0100

    Execution via docker containers!

--- src/exec/__init__.py
+++ src/exec/__init__.py
@@ -1,11 +1,20 @@
+import os
 import subprocess
 import random
+import docker
 
 def execute_command(command: str):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    output, error = process.communicate()
-    print(error)
-    return (output, error)
+    client = docker.from_env()
+    container = client.containers.run(
+        image=os.getenv('DOCKER'),
+        command=command,
+        detach=True,
+        remove=True,
+        cpu_shares=1024,
+        mem_limit='1g'
+    )
+    
+    return (container.logs().decode("utf-8"), "")
 
 def get_file(code: str):
     filename = f"./snowbot-{random.randint(100, 500)}-source.sn"
