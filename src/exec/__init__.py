311f2a32a15fdf36485539b7b7032419fbd15639
mauro-balades <mauro.balades@tutanota.com>
2023-03-25 18:05:24 +0100

    Created output embeds

--- src/exec/__init__.py
+++ src/exec/__init__.py
@@ -1,20 +1,25 @@
 import os
-import subprocess
 import random
 import docker
+from strip_ansi import strip_ansi
 
 def execute_command(command: str):
-    client = docker.from_env()
-    container = client.containers.run(
-        image=os.getenv('DOCKER'),
-        command=command,
-        detach=True,
-        remove=True,
-        cpu_shares=1024,
-        mem_limit='1g'
-    )
-    
-    return (container.logs().decode("utf-8"), "")
+    try:
+        client = docker.from_env()
+        container = client.containers.run(
+            image=os.getenv('DOCKER'),
+            command=command,
+            detach=True,
+            remove=True,
+            cpu_shares=1024,
+            mem_limit='1g'
+        )
+
+        out = strip_ansi(container.logs().decode("utf-8"))
+
+        return (out, "")
+    except Exception as e:
+        return ("", str(e))
 
 def get_file(code: str):
     filename = f"./snowbot-{random.randint(100, 500)}-source.sn"
--- src/modals/code.py
+++ src/modals/code.py
@@ -1,3 +1,4 @@
+import datetime
 import discord
 from src.exec import execute_code
 
@@ -10,6 +11,34 @@ class CodeModal(discord.ui.Modal):
     async def callback(self, interaction: discord.Interaction):
         output, error = execute_code(self.children[0].value)
         if error:
-            await interaction.response.send_message("Compilation failed: " + error.decode("utf-8"))
+            await interaction.response.send_message("", embed=self._create_error_embed(error))
         else:
-            await interaction.response.send_message("Compilation successful: " + output.decode("utf-8"))
+            await interaction.response.send_message(": ", embed=self._create_output_embed(output, interaction))
+
+    def _create_error_embed(error: str, author_name: str):
+        embed = discord.Embed(
+            title="Compilation failed!",
+            description="There has been an error while compiling your code.",
+            color=discord.Colour.red(),
+            timestamp=datetime.datetime.now(datetime.timezone.utc)
+        )
+
+        embed.add_field(name="Error message", value=f"```rs\n{error}```", inline=False)
+        embed.set_footer(text="Fix your code and try again!")
+
+        return embed
+
+    def _create_output_embed(output: str, ctx: discord.Interaction):
+        embed = discord.Embed(colour=discord.Colour.dark_grey(), timestamp=datetime.datetime.now(datetime.timezone.utc))
+        embed.set_author(name=f"{str(ctx.message.author)}'s code execution", icon_url=ctx.message.author.avatar_url)
+
+        if len(output) > 300 or output.count("\n") > 10:
+            embed.description = f"Output too large - [Full output](TODO)"
+
+            if output.count("\n") > 10:
+                output = "\n".join(output.split("\n")[:10]) + "\n(...)"
+            else:
+                output = output[:300] + "\n(...)"
+
+        embed.add_field(name="Output", value=f"```yaml\n{output}```", inline=False)
+        return embed
