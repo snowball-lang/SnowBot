31f032d7b7271295557eb90883f0970ccad3116e
mauro-balades <mauro.balades@tutanota.com>
2023-03-26 10:34:41 +0200

    Sucessfully executing snowball

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt-get -y update
 RUN apt-get install sudo -y
 RUN apt-get install curl -y
 RUN apt-get install python3 python3-pip -y
+
 WORKDIR /app
 
 COPY main.py requirements.txt /app
@@ -14,7 +15,6 @@ RUN sudo mv /bin/sh.bash /bin/sh
 RUN touch ~/.bashrc
 # TODO: use the website URL in the future
 RUN curl -fsSL https://raw.githubusercontent.com/snowball-lang/snowball/dev/scripts/install.sh | bash -s -- -y
+
 RUN pip install -r requirements.txt
 RUN eval "$(docker-machine env default)"
-
-
--- src/exec/__init__.py
+++ src/exec/__init__.py
@@ -5,38 +5,44 @@ import docker
 from strip_ansi import strip_ansi
 
 SNOWBALL = "/root/.snowball/bin/snowball"
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) + "/../.."
+BOT_ROOT = "/app"
 
 def execute_command(command: str, id: int):
     try:
         client = docker.from_env()
 
-        container = client.containers.run(
-            image=os.environ.get("DOCKER"),
-            command=command,
-            volumes={f"{os.getcwd()}/code": {'bind': '/app/code', 'mode': 'rw'}},
+        container = box = client.containers.run(image = os.environ.get("DOCKER"),
+                            detach = True,
+                            tty = True,
+                            volumes={f"{os.getcwd()}/code": {'bind': '/app/code', 'mode': 'rw'}},
+
+                            command = "/bin/bash")
+        exit_code, logs = container.exec_run(
+            command,
         )
 
-        # Print the container output
-        logs = container.decode('utf-8')
-
-        return (logs, 0)
+        logs = strip_ansi(logs.decode("utf-8").replace("\\n", "\n").replace("\\t", "\t"))
+        return (logs, exit_code)
     except Exception as e:
         return (str(e), 1)
 
 def get_file(code: str, id: int):
-    filename = f"./code/snowbot-{id}-source.sn"
-    with open(filename, "a+") as f:
+    filename = f"code/snowbot-{id}-source.sn"
+    root_file = f"{ROOT_DIR}//{filename}"
+    bot_file = f"{BOT_ROOT}/{filename}"
+
+    with open(root_file, "a+") as f:
         f.write(code)
-    return filename
+    return bot_file, root_file
 
 def execute_code(code: str):
     id = random.randint(100, 500)
-    f = get_file(code, id)
-    files = [f for f in os.listdir(".") if os.path.isfile(f)]
+    bot_file, main_file = get_file(code, id)
 
-    print(files)
-    command = ' '.join([SNOWBALL, "run", "-f", f])
-    os.remove(f)
+    command = ' '.join([SNOWBALL, "run", "-s", "-f", bot_file])
 
-    return execute_command(command, id)
+    c = execute_command(command, id)
+    os.remove(main_file)
 
+    return c
